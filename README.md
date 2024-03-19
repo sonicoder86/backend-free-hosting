a944e405b31e58757544ebeb27195835871aad88
vuesomedev <soos.gabor86@gmail.com>
2024-03-19 19:40:25 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Backend Free Hosting
+
+Showcasing free hosting for frontend applications (static hosting).
+
+## Render
+
+https://render.com/
+
+- Install Githubb App, authorize
+
+[//]: # (- https://sonicoder86.github.io/frontend-free-hosting/)
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
+const app = new Hono()
+
+app.get('/', (c) => c.text('Hono!'))
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
