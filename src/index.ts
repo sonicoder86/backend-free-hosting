ebc5a4b32faefccfe6a609258633617b33bc3866
vuesomedev <soos.gabor86@gmail.com>
2024-03-22 20:28:54 +0100

    add deno

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = new Hono()
 
 app.get('/', (c) => c.text('Hello World!'))
 
-const port = parseInt(process.env.PORT || '') || 3000
+const port = parseInt(Deno.env.get('PORT') || '') || 3000
 console.log(`Server is running on port ${port}`)
 
 serve({
